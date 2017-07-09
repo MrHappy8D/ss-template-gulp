308215ba2d82f3a20de3912992b1cc715d43851c
Christian Dixon-McIver <christian.dixonmciver@gmail.com>
2017-07-09 23:07:06 +1200

    Restarting gulp install

--- gulpfile.js/tasks/_production.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp        = require('gulp');
-var runSequence = require('run-sequence');
-
-// Run this to compress all the things!
-gulp.task('production', function(){
-  runSequence('default', ['minifyCss', 'uglifyJs'], 'size-report');
-});
